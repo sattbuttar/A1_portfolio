190ca6e152a31c5ba4ff40fb11ad70553670df74
Satwinder Singh <ssingh1617@conestogac.on.ca>
2020-09-28 01:38:36 -0400

    imgs

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"category1.md", "specialImage":"/img/FavouriteFoodBaskets750x500_xraj9x.png"},
-{"id": "item2", "fname":"category2.md", "specialImage":"/img/FavouriteDay750x500_pdni4p.png"},
-{"id": "item3", "fname":"category3.md", "specialImage":"/img/twiliobot750x500_z7anzp.png"},
-{"id": "item4", "fname":"category4.md", "specialImage":"/img/A1Portfolio.png"}];
+export default [{"id": "item1", "fname":"category1.md", "specialImage":"/img/A1Portfolio.png"},
+{"id": "item2", "fname":"category2.md", "specialImage":"/img/A1Prototypes.png"},
+{"id": "item3", "fname":"category3.md", "specialImage":"/img/A1Programming.png"},
+{"id": "item4", "fname":"category4.md", "specialImage":"/img/A1UX.png"}];
