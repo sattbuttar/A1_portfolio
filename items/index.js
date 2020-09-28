8c4767e5bc809b6a395d74b1a0379c72622e4a34
Satwinder Singh <ssingh1617@conestogac.on.ca>
2020-09-28 01:41:08 -0400

    a

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
 export default [{"id": "item1", "fname":"category1.md", "specialImage":"/img/A1Portfolio.png"},
-{"id": "item2", "fname":"category2.md", "specialImage":"/img/A1Prototypes.png"},
+{"id": "item2", "fname":"category2.md", "specialImage":"/img/A1Prototypess.png"},
 {"id": "item3", "fname":"category3.md", "specialImage":"/img/A1Programming.png"},
 {"id": "item4", "fname":"category4.md", "specialImage":"/img/A1UX.png"}];
